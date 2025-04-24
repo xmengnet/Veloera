fd7265c6da2a71d77c0ba5555165f5372bd2c4c4
Tethys Neuroplexus <isneuroplexus@duck.com>
2025-04-24 20:03:28 +1000

    chore: Fix proxy path in Vite config

--- web/vite.config.js
+++ web/vite.config.js
@@ -59,11 +59,11 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3909',
         changeOrigin: true,
       },
       '/pg': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3909',
         changeOrigin: true,
       },
     },
