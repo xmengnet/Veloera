ca8d7645c2da6b8d42b3b77179dbb610392c711c
Tethys Neuroplexus <isneuroplexus@duck.com>
2025-05-24 16:48:48 +1000

    fix: fix cannot get max user id

--- model/user.go
+++ model/user.go
@@ -110,7 +110,7 @@ func CheckUserExistOrDeleted(username string, email string) (bool, error) {
 
 func GetMaxUserId() int {
 	var user User
-	DB.Last(&user)
+	DB.Unscoped().Last(&user)
 	return user.Id
 }
 
